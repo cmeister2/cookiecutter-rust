695a0d82f0b115bb2d22224a18a138336df0e18a
Max Dymond <cmeister2@gmail.com>
2022-04-21 22:14:21 +0100

    Initial commit

---
+++ {{cookiecutter.project_name}}/src/lib.rs
@@ -0,0 +1,10 @@
+//! {{cookiecutter.short_description}}
+#![deny(
+    missing_docs,
+    trivial_casts,
+    trivial_numeric_casts,
+    unused_extern_crates,
+    unused_import_braces,
+    unused_qualifications,
+    unused_results
+)]
